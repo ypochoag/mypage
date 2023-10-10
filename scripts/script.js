485d41d220dff7e85a3d22c795c5beef2129ce44
Yuri Ochoa <ypochoag@gmail.com>
2023-10-10 17:10:54 -0500

    commit study section

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,38 @@
-alert("Bienvenido a mi page web");
+let menu_responsive = document.querySelector(".checkbtn");
+
+menu_responsive.onclick = function () {
+    navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+};
+
+
+const form = document.getElementById("form");
+const nombre = document.getElementById("nombre");
+const parrafo = document.getElementById("alertas");
+
+function validarFormulario() {
+  let warnings = "";
+  let valido = true;
+  parrafo.innerHTML = "";
+
+  if (nombre.value.length < 4) {
+    warnings += `El nombre debe contener más de 4 carcateres`;
+    valido = false;
+  }
+
+  if (!valido) {
+    parrafo.innerHTML = warnings;
+  } else {
+    parrafo.innerHTML = "Enviado";
+  }
+  return valido;
+}
+
+form.addEventListener("submit", (e) => {
+  if (validarFormulario()) {
+    // Si la validación es exitosa, puedes enviar el formulario
+    formulario.submit();
+  } else {
+    e.preventDefault(); // Evita que el formulario se envíe automáticamente
+  }
+});
